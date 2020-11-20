147f3281db41754b621780b46e88d9246561a1ff
raoqinhao <976869901@qq.com>
2020-11-20 12:59:49 +0800

    解决冲突

--- user-service/src/test/java/com/hh/userservice/test/TestBigDecimal.java
+++ user-service/src/test/java/com/hh/userservice/test/TestBigDecimal.java
@@ -17,17 +17,6 @@ public class TestBigDecimal {
     }
 
     @Test
-    public void testSubtractBigDecimal() {
-        BigDecimal bigDecimal = new BigDecimal("12.33251");
-        BigDecimal bigDecima2 = new BigDecimal("6.47251");
-        BigDecimal result1 = bigDecimal.subtract(bigDecima2).setScale(1, BigDecimal.ROUND_HALF_UP);
-        System.out.println(result1);
-        BigDecimal result2 = bigDecimal.subtract(bigDecima2).setScale(1, BigDecimal.ROUND_HALF_UP);
-        System.out.println(result2);
-
-    }
-
-    @Test
     public void testMultiplyBigDecimal() {
         BigDecimal bigDecimal = new BigDecimal("11.111");
         BigDecimal bigDecima2 = new BigDecimal("1.11");
