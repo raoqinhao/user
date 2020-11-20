eff57b399d079d4fbaaa42ccd1a72b390299c2ff
raoqinhao <976869901@qq.com>
2020-11-20 15:20:50 +0800

    修改BigDecimal数据

--- user-service/src/test/java/com/hh/userservice/test/TestBigDecimal.java
+++ user-service/src/test/java/com/hh/userservice/test/TestBigDecimal.java
@@ -30,7 +30,7 @@ public class TestBigDecimal {
     public void testSubtractBigDecimal() {
         BigDecimal bigDecimal = new BigDecimal("12.33251");
         BigDecimal bigDecima2 = new BigDecimal("6.47251");
-        BigDecimal result1 = bigDecimal.subtract(bigDecima2).setScale(1, BigDecimal.ROUND_HALF_UP);
+        BigDecimal result1 = bigDecimal.subtract(bigDecima2);
         System.out.println(result1);
         BigDecimal result2 = bigDecimal.subtract(bigDecima2).setScale(1, BigDecimal.ROUND_HALF_UP);
         System.out.println(result2);
