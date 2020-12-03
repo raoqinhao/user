011eaf3474bc553f34b83a2151171a04eab5f22c
raoqinhao <976869901@qq.com>
2020-12-03 09:15:34 +0800

    dev1分支添加Lambda中的数据。

--- user-service/src/test/java/com/hh/userservice/TestLambda.java
+++ user-service/src/test/java/com/hh/userservice/TestLambda.java
@@ -70,6 +70,7 @@ public class TestLambda {
         System.out.println("new dev11");
         System.out.println("new dev111");
         System.out.println("new dev1111");
+        System.out.println("cs");
     }
 
     @Test
