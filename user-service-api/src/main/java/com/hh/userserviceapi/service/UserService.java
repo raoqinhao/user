bd4ffa570fd2e0eed133d6dfb44225f6c1ddbac7
raoqinhao <976869901@qq.com>
2020-12-29 13:28:22 +0800

    添加openfeign接口,以及相关依赖。

--- user-service-api/src/main/java/com/hh/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/com/hh/userserviceapi/service/UserService.java
@@ -1,5 +1,17 @@
 package com.hh.userserviceapi.service;
 
+import com.hh.userserviceapi.pojo.UserBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@FeignClient("USER-SERVICE-PROVIDE")
+@Component
 public interface UserService {
+
+    @RequestMapping(value = "/findUserInfo/{id}", method = RequestMethod.GET)
+    UserBean findUserInfo(@PathVariable String id);
+
 }
