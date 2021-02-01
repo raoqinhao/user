f036479d48c2958d2d1078d9e512f2b44834182c
raoqinhao <976869901@qq.com>
2021-02-01 21:08:31 +0800

    添加post请求，以及alibaba 的json依赖。

--- user-service-consume/src/main/java/com/hh/userserviceconsume/controller/UserController.java
+++ user-service-consume/src/main/java/com/hh/userserviceconsume/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.hh.userserviceconsume.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.hh.userserviceapi.pojo.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -20,11 +19,23 @@ public class UserController {
 
     private static String requestPath = "http://USER-SERVICE-PROVIDE";
 
-    @RequestMapping(value = "/consume/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/get/consume/{id}", method = RequestMethod.GET)
     @ResponseBody
     public UserBean getConsumeUserBean(@PathVariable String id) {
         return restTemplate.getForObject(requestPath + "/findUserInfo/" + id, UserBean.class);
     }
 
+    @RequestMapping(value = "/post/consume", method = RequestMethod.POST)
+    @ResponseBody
+    public UserBean postConsumeUserBean(@RequestBody String json) {
+        try {
+            JSONObject jsonObject = JSON.parseObject(json);
+            String id = jsonObject.getString("id");
+            return restTemplate.getForObject(requestPath + "/findUserInfo/" + id, UserBean.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
