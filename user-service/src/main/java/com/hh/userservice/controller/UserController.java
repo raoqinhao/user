bbd61fc2c6ca34f97383b884f9c2f599acc01ba5
raoqinhao <976869901@qq.com>
2021-02-08 09:37:29 +0800

    整理数据

--- user-service/src/main/java/com/hh/userservice/controller/UserController.java
+++ user-service/src/main/java/com/hh/userservice/controller/UserController.java
@@ -19,13 +19,11 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
