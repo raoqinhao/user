7129efd4b363b2ceda9b952f138ecf6bb599346e
raoqinhao <976869901@qq.com>
2021-02-20 15:50:52 +0800

    整合mybatis成功。

---
+++ user-service-provide02/src/main/java/com/hh/userserviceprovide02/mapper/UserBeanMapper.java
@@ -0,0 +1,8 @@
+package com.hh.userserviceprovide02.mapper;
+
+
+import com.hh.userserviceprovide02.pojo.UserBean;
+
+public interface UserBeanMapper {
+    int insert(UserBean userBean);
+}
---
+++ user-service-provide02/src/main/java/com/hh/userserviceprovide02/service/UserBeanService.java
@@ -0,0 +1,9 @@
+package com.hh.userserviceprovide02.service;
+
+import com.hh.userserviceprovide02.pojo.UserBean;
+
+public interface UserBeanService {
+
+    String saveUserInfo(UserBean userBean);
+
+}
