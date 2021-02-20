5e5942d277824980a0e9014ea50cb8c0c7cf28b3
raoqinhao <976869901@qq.com>
2021-02-20 22:31:34 +0800

    添加RestTemplate远程服务调用模板，已经远程调用接口。

--- user-service-provide02/src/main/java/com/hh/userserviceprovide02/controller/UserController.java
+++ user-service-provide02/src/main/java/com/hh/userserviceprovide02/controller/UserController.java
@@ -5,12 +5,18 @@ import com.hh.userserviceprovide02.service.UserBeanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserBeanService userBeanService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
 
     @RequestMapping(value = "/findUserInfo/{id}", method = RequestMethod.GET)
@@ -23,6 +29,21 @@ public class UserController {
     @ResponseBody
     public String saveUserInfo(@RequestBody UserBean userBean) {
         return userBeanService.saveUserInfo(userBean);
+    }
+
+    @RequestMapping(value = "saveUserInfoAndSaveRemoteInfo",method = RequestMethod.POST)
+    public String saveUserInfoAndSaveRemoteInfo(@RequestBody UserBean userBean) {
+        try {
+            String saveUserInfo = userBeanService.saveUserInfo(userBean);
+            userBean.setId(UUID.randomUUID().toString().replaceAll("-",""));
+            userBean.setUsername(userBean.getUsername() + "r");
+            String remoteStatus = restTemplate.postForObject("http://localhost:8087/mybatisplus/saveUserBean", userBean, String.class);
+            System.out.println(remoteStatus);
+            return saveUserInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail";
+        }
     }
 
 }
--- user-service-provide03/src/main/java/com/hh/userserviceprovide03/controller/UserController.java
+++ user-service-provide03/src/main/java/com/hh/userserviceprovide03/controller/UserController.java
@@ -4,12 +4,18 @@ import com.hh.userserviceprovide03.pojo.UserBean;
 import com.hh.userserviceprovide03.service.UserBeanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserBeanService userBeanService;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping(value = "/findUserInfo/{id}", method = RequestMethod.GET)
     public UserBean getUserInfo(@PathVariable("id") String id) {
@@ -19,6 +25,21 @@ public class UserController {
     @RequestMapping(value = "/saveUserInfo", method = RequestMethod.POST)
     public String saveUserInfo(@RequestBody UserBean userBean) {
         return userBeanService.saveUserInfo(userBean);
+    }
+
+    @RequestMapping(value = "saveUserInfoAndSaveRemoteInfo",method = RequestMethod.POST)
+    public String saveUserInfoAndSaveRemoteInfo(@RequestBody UserBean userBean) {
+        try {
+            String saveUserInfo = userBeanService.saveUserInfo(userBean);
+            userBean.setId(UUID.randomUUID().toString().replaceAll("-",""));
+            userBean.setUsername(userBean.getUsername() + "r");
+            String remoteStatus = restTemplate.postForObject("http://localhost:8087/mybatisplus/saveUserBean", userBean, String.class);
+            System.out.println(remoteStatus);
+            return saveUserInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail";
+        }
     }
 
 }
